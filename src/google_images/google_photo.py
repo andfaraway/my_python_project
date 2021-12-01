697f7109b7efcfd925867f6e0731077a245508c4
libin <bin.li@imin.sg>
2021-12-01 10:02:41 +0800

    bug fix

--- src/google_images/google_photo.py
+++ src/google_images/google_photo.py
@@ -16,13 +16,11 @@ def request_download(image_url, directory, filename):
     path = here + '/' + directory + '/' + filename
     if directory is None or directory == '':
         path = here + '/' + filename
-    print(path)
+
     try:
         if 'data:image' in image_url:
             # base64转图片
-            image_url = image_url.strip()
-            image_url = image_url + '==='
-            print('%d:%s' % (len(image_url), image_url))
+            image_url = image_url.split('base64,')[1]
             _img_data = base64.b64decode(image_url)
             with open(path, 'wb') as f:
                 f.write(_img_data)
@@ -33,8 +31,11 @@ def request_download(image_url, directory, filename):
                 f.write(r.content)
 
         print('下载成功--> %s' % image_url)
+        return True
     except IOError as error:
         print('下载失败--> %s' % image_url)
+        print(error)
+        return False
 
 
 # 下载大图url
@@ -49,7 +50,7 @@ def down_load_big_img(pic_name):
     img_url = html.get_property('src')
 
     # 获取图片后缀
-    suffix = 'jpg'
+    suffix = 'png'
     if 'data:image' in img_url:
         # base64文件
         suffix = img_url.split(';')[0].replace('data:image/', '')
@@ -58,7 +59,10 @@ def down_load_big_img(pic_name):
         original_url = img_url
         if '?' in img_url:
             original_url = img_url.split('?')[0]
-        suffix = original_url.split('.')[-1]
+            suffix = original_url.split('.')[-1]
+            if '/' in suffix:
+                suffix = 'png'
+
     elif 'fmt=' in img_url:
         # 带有格式的url
         pattern = re.compile(r'fmt=[a-zA-z0-9]+&')
@@ -72,7 +76,16 @@ def down_load_big_img(pic_name):
     # 创建存储目录
     os.makedirs(key_word, exist_ok=True)
     # 开始下载
-    request_download(img_url, key_word, save_name)
+    success = request_download(img_url, key_word, save_name)
+
+    # url写入text
+    try:
+        if success:
+            path = here + '/' + key_word + '/urls.text'
+            with open(path, 'a') as f:
+                f.write(img_url + '\n')
+    except IOError:
+        print('url write error')
     return img_url
 
 
@@ -110,25 +123,22 @@ def get_image_url():
 
 
 def test():
-    bs = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAR4AAACwCAMAAADudvHOAAAA/FBMVEX/////8+YAAAD+9eb0re/0ru3/8+f/9' \
-         '+j/++v/+Ov/9OTn5+ft7e3/++3//OwAAALz8/P5' \
-         '+fna2trOzs6amppTU1PHx8d5eXlCQkKPj48oKCixsbEWFhY9PT3h4eGkpKQgICBhYWHU1NS8vLxxcXGCgoIuLi5JSUlpaWnNx7' \
-         '+dnZ22trbz6+AzMzMNDQ1ON0+rgKe4jbReRF4vJDOHaYyWdJl7Ynv9uPuwfquzea39r/mMaITJl8dBMD' \
-         '/fo97lq99wUm0bCx0jFiFoR2fQocwsISj1tvBbRFdsU3HGvLWXkYnc08SnoppYU0qPjYRZWVm6tKm2rKQsJx95cmgglVlRAAAIfUlEQVR4nO2d+1caSRbH6aq2X1rtIyKKgKDyyjTMZjZZdtzJYyZZdgCjEv///2XurYKmCoVxf7E41P3knMRGc06d76m672oLBYIgCIIgCIIgCIIgCIIgCIIgCIIgCOK1ObS9gA3mvNhnrbczhU5Kp7322ZHdFW0QRy0mKV/iQ1s9sGvaT5I3LOe8cLx4qNpe2Eawh1L8/I93v0hJ8K9/vv/wr334t2l7aZtAA4T4MPD9wb8Z24c/7NfhzY0//Bk+3rW9Nvvsggz/GewAg99Qn4+ffMlnEOrM9uLscw7yfPZBHf/G/8TYL8MbJc/gC2OnthdnnzOQRG4e1OTTr76/o+Tx4awxcl4lxr7k8ujAUWN7tldnnSZjX0melVww9vvAX5IHHm/ocCGXoMJvy7sHHocQBxVtL24DaM1ssymPPFvntte2AcDpYu9udgz8neFH+Nj20jaCCmN/DE15dgYfaPPMOAElvhr6+P43iJ8pJVVcy7zCX8hzM/wvfFSzva5NoQxivB9ohud/8MGF7VVtDDUsY3wZ+kN5xPzBn/DYsL2oDeII9fn4WR0wTEbJ8Bjs1rHS826wMxwOce/UKV42OCziBno/vAF19ln9wPZ6No4zecC+4ckqUyr6lJN5Db5F6jzHgerh9MjurADbOFRBXU2tU5p/dfKGGhWFw4Nnj9I5BtLs9Oq1l7NZnDQqrNJ4s/zxYWNupa9trGpDyFUomq7qoLXoJJdW/N/t56CuN9c1qljNmETeCH/g0tbybINuvDXtTltLpwjLh4045DzO+ox17C3QKtge7cRRGIVjUx94aIuEA9GtuxVV2DSnoYeIbl2zMrh5JiHHb8QTZ3tdOHrQVfJ4cfaD5QMHVThaKZfyeJmzRUPIsPazRMnDw6w+rw/WwC53YyUPnzg7xYIJ6FwezwtxnzCMAiFxr/OZag7bHiygTnJ5kgg3CjsqFHpghaKZPOl3dz0XqDEOc3mSaIT67NU0k8Qzh5tdJcbKsbdA4EmqwtnqB8HsxE3hE1frhjiQOtL1Se9meUSkzlwgIHC8t71Ma5Tl9uG5PInoYD0ezlayOFvu5uxXaH0E1/ZPfIq7J+ZKHhlNu3q2gKKMjzV5Ahn+PAglT9xyOWFXgXMvSJLFBlLuvRthVBh24csnpSCXOAdbUxI8j34gCUVNVMqVgmtr2V6hXbAeNhKaPDx6wB5pFnIe9Bk7tr1AuxziXsl068xTNMjVIAlHzuZbCzD4qerBD+cpjvvcC/Dy1NORreNHocvD4yKYpGbcpxGfgvLuD1Ggb6Cgh/vH2UqYwUEfXZV+vhKsMWP43La9tk0AzU/PMwgnT9sXzvIWC/FGcuGp8Idu20rQzNwuoh9EVjccDwpzKloNbK5PE8wP3amQ1J5Eh6APxD102VbxE5pnUx8eYHXjre2VbQZncJQascc1hYIMz9yJ7ZVtBpicQvKubyDl3t3sAj4BL/jfpsb5SjF7pxuBhcLRcRMTUTaKdHkScO/7lJceneZzPkZ2Ae6r5PQElCSfZ8Zx+ExPTr0g/M4cT9zldZN657rdR33apncPecXlCbHCzCZHIhYeNkWN4g84eum+KrbXaI8r6bGSIEg8keG9nLFZHEtRNFfnEORNyep82olLfR5SPTtNxDV85myztMfYeC6PmpZjXcN9BVnZ4W5pGS1PHi3LQk8/M/SR/UBX579bhjyeHAirBoHuwGT046j3ajN2p8nDRVP2cXTzE+CohnvB897l1VWtJE3zwlcF4h5M8V2qyxNN3RtleVPMo+VuqMfKQQ/0uTWyLwEGvGd7wa+K9oJCVuW6PCHeFTDdVzxyrPRzvhBnH8+SZmsS1acwioc46+PQxf8DFGCaBUH2gEdpKdQRWOi5fTKX6U7scyynnBIgijtyrxj6gHuvTPQDl6Dz+sn2ql+NXp57Jol4xPtbMV949yCIsoAbja+06FBouGc0tuIG6NMxC81LBNHYoboqNrYWFyq4h0WNcbpOHqcmDaU8i7aounAyFt7KDRQ4dYFgzwxsVNELh5zXbKCeQ6+YhTx9vIiLOVeVjHXnC3INJ2zz0cXx+d41Yy0jbVATzbfxKnVkz337i4Y19RoRLLA/GCOXSp9gpTyxC579Ssu0mN604VxMcEZ+te1pbf+Ys3xrfv2+wuTs4Hdz4jLMplm4QpsgmTiQlJalfRFeVx2xR72qA1FzuNqxQ2i99WEhHi3ZhwhjebWfNYUXaPLwlfLIO8nbbpnBX32fdSVkBYexu3SNtdFIOw6Um6sY7Sh5IOksq1DnJfrIsGjrX2VY1cruSRg0VKic/J1AgSeqDhhmHAF71LoSsbxb+xgl67Tx5qX47S8WvmXshyZPIqZP/NezyDGN7b9ggWHPZNGVSBIhq8qjVdHO/Kce3SgVHsIuuBO6qZGpxJpMC9VJpxBEbn0+gTSXOxCQanWa4TrbnEivVbe98ldhV5qaRLfFPI7XmuYkRq/lyO2TpupQrNNjCWl4tj0XnXPYZ6yS8ZfrE6Nzc+fqCY6hyhenvUwdabr77vT/CiVwQ4/pC+VRNdZtz7UM2jITfZE88aTiRsSjIV92OY5ecL6EVMcVszxnV1Uy/k6eQA1quHfjraYyrbWpBOydqZvqzPRpZ+Fq/56Eobyi49rJUshfS1GZiOf1CSCXV78I2LF5whz11vyHFaWMMLyVzTBHUonnKEkDHRvJuqrGJ+FEXvBqOBQNPgXfrMuKmViWJ5zIcTG3b3EV1EtlsdizyFCTJIy7j1KcU9ffbFQo7KlLkuOJEFEUCSGyUWnWX3Zlkmc9Z0qMemM8vh2XqvPm+/GWt0NfTC3/lQs5vQsSZ8FRydDm2GFnvoLLi9J9q1ztnF1tf7OGIAiCIAiCIAiCIAiCIAiCIAiCIAiCIAiCIP5P/gLGAKgUV6MNpQAAAABJRU5ErkJggg== '
-    request_download(bs,  '', 'test.png')
+    image_url = 'https://inews.gtimg.com/newsapp_bt/0/11929928804/1000'
+    image_url = 'https://lh3.googleusercontent.com/proxy/YrtE8YhnqS4gym-yrMKzK3Z0AsZ-nuEYOGyx0IMO7blup-mhtg7maNaMg3nC_WdRcrSmqueBfq5JiQmnfhHZjbznFOHH6CM-UOyGc_-Fpb0TtyaJo8LUgKcSRpY1zznpWGkXFemgpsUvaBycsU52xHQ'
+    request_download(image_url, '', 'test.png')
     quit()
 
 
 if __name__ == '__main__':
-    test()
+    # test()
 
     # 创建一个参数对象，用来控制chrome是否以无界面模式打开
     ch_op = Options()
     # 设置谷歌浏览器的页面无可视化，如果需要可视化请注释这两行代码
-    # ch_op.add_argument('--headless')
-    # ch_op.add_argument('--disable-gpu')
+    ch_op.add_argument('--headless')
+    ch_op.add_argument('--disable-gpu')
 
-    key_word = '比心'
+    key_word = '鹿晗'
     num = 10
     start = 1
     url = "https://www.google.com/search?&tbm=isch&q=%s&num=%d&start=%d&tbs=isz:l" % (
@@ -136,8 +146,15 @@ if __name__ == '__main__':
     driver = webdriver.Chrome(service=Service('./chromedriver'), options=ch_op)
     driver.get(url)
 
-    total_count = 100
+    total_count = 200
     images = []
+    get_image_url()
 
-    image_urls = get_image_url()
+    key_word = '原神'
+    images.clear()
+    get_image_url()
+
+    key_word = '英雄联盟壁纸'
+    images.clear()
+    get_image_url()
     driver.__exit__()
