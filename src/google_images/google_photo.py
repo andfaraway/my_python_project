fb235716436fa6bd64ced5c04eaa30daf880d2ef
libin <bin.li@imin.sg>
2021-12-02 14:37:08 +0800

    添加重新启动继续上一次下载功能

--- src/google_images/google_photo.py
+++ src/google_images/google_photo.py
@@ -5,6 +5,7 @@ import time
 
 import requests
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -40,9 +41,13 @@ def down_load_big_img(pic_name):
     div_show = driver.find_element(by=By.ID, value='islsp')
 
     #  找到大图位置，位置可以右键元素 COPY XPATH
-    html = div_show.find_element(by=By.XPATH,
-                                 value='//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img')
-    img_url = html.get_property('src')
+    try:
+        html = div_show.find_element(by=By.XPATH,
+                                     value='//*[@id="Sva75c"]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img')
+        img_url = html.get_property('src')
+    except Exception as error:
+        print('element no found')
+        return 'error element'
 
     # 获取图片后缀
     suffix = 'png'
@@ -95,27 +100,37 @@ def get_image_url():
     div_boxs = islrc_box.find_elements(by=By.XPATH, value='./*')
     normal_class = div_boxs[0].get_attribute('class')
     count = len(div_boxs)
-    for index in range(len(images), count):
+    # 从第几张图开始下载
+    begin_index = get_begin_index(download_path)
+    print('begin_index = {},count={}'.format(begin_index, count))
+    if begin_index >= count:
+        scroll_to_bottom()
+        get_image_url()
+        return images
+    print('begin_index = {},count={}'.format(begin_index, count))
+    for index in range(begin_index, count):
         div_box = div_boxs[index]
         if normal_class != div_box.get_attribute('class'):
             print('非正常div，跳过: %s', div_box.get_attribute('class'))
             continue
         print('%s%s(%d/%d)开始下载' % (current_time(), key_word, index, count))
+
+        img = div_box.find_element(by=By.TAG_NAME, value='img')
         try:
-            img = div_box.find_element(by=By.TAG_NAME, value='img')
             img.click()
-            time.sleep(1)
-            img_url = down_load_big_img('%d' % index)
-            # img_url = image.get_attribute("src") #缩略图
-            images.append(img_url)
-            if len(images) > total_count:
-                break
-        except BaseException as error:
-            print(error.__class__)
+        except Exception as error:
+            print('img不能点击')
+            continue
+        time.sleep(1)
+        img_url = down_load_big_img('%d' % index)
+        # img_url = image.get_attribute("src") #缩略图
+        images.append(img_url)
+        if len(images) > total_count:
+            return images
+
     # 滚动页面
     if len(images) < total_count:
-        time.sleep(2)
-        driver.execute_script('return document.body.scrollHeight')
+        scroll_to_bottom()
         get_image_url()
     return images
 
@@ -124,44 +139,60 @@ def current_time():
     return time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime())
 
 
+# 获取最后下载图片的index
+def get_begin_index(path):
+    count = 0
+    if os.path.exists(path):
+        try:
+            with open(path + '/urls.text', 'r') as f:
+                list1 = f.read().split('\n')
+                count_text = list1[-2].split('.')[0].replace('(', '')
+                count = int(count_text) + 1
+        except Exception as error:
+            count = 0
+            print(error)
+    return count
+
+
+# 滚动到页面底部
+def scroll_to_bottom():
+    js = 'window.scrollTo(0,document.body.scrollHeight)'
+    driver.execute_script(js)
+    time.sleep(3)
+
+
 if __name__ == '__main__':
+
     # 存储路径
     download_root_path = '/Users/libin/Desktop/downloads/'
     download_path = download_root_path
+
     # 创建一个参数对象，用来控制chrome是否以无界面模式打开
     ch_op = Options()
     # 设置谷歌浏览器的页面无可视化，如果需要可视化请注释这两行代码
     ch_op.add_argument('--headless')
     ch_op.add_argument('--disable-gpu')
-    try:
-        driver = webdriver.Chrome(service=Service('./chromedriver'), options=ch_op)
-    except Exception as error:
-        print(error)
-        quit()
+
     num = 10
     start = 1
     total_count = 200
     images = []
 
-    downloads_list = ['日落', '海', '阳光', '壁纸']
+    downloads_list = ['美女']
 
-    print('{}开始下载:{}'.format(current_time(), downloads_list))
+    print('{}:{}'.format(current_time(), downloads_list))
     while len(downloads_list):
         key_word = downloads_list[0]
-        # noinspection PyBroadException
-        try:
-            download_path = download_root_path + key_word
-            if not os.path.exists(download_path):
-                os.mkdir(download_path)
-            url = "https://www.google.com/search?&tbm=isch&q=%s&num=%d&start=%d&tbs=isz:l" % (
-                key_word, num, start)
 
-            driver.get(url)
-            images.clear()
-            get_image_url()
-            print('{}下载完成。'.format(key_word))
-            downloads_list.pop(0)
-        except Exception as error:
-            print('下载({})出错'.format(key_word))
+        download_path = download_root_path + key_word
+        if not os.path.exists(download_path):
+            os.mkdir(download_path)
+        url = "https://www.google.com/search?&tbm=isch&q=%s&num=%d&start=%d" % (
+            key_word, num, start)
+        driver = webdriver.Chrome(service=Service('./chromedriver'), options=ch_op)
+        driver.get(url)
+        images.clear()
+        get_image_url()
+        print('{}下载完成。'.format(key_word))
+        downloads_list.pop(0)
     print(current_time() + '下载完成')
-    quit()
